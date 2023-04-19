a35937656257bdbf6ec2bb0fff98c369d4ea816a
Dan97O <dan97-oddo@live.it>
2023-04-19 12:13:15 +0200

    add input

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <script>
+import { store } from './store';
 export default {
+  data() {
 
-
+    return {
+      store
+    }
+  },
+  mounted() {
+    this.store.callApi(this.store.API_URL + this.store.searchMovies)
+  }
 }
 </script>
 
-<template></template>
+<template>
+  <div>
+    <input type="text" v-model="store.searchMovies">
+  </div>
+</template>
 
 <style></style>
--- src/store.js
+++ src/store.js
@@ -4,9 +4,22 @@ import { reactive } from "vue";
 
 
 export const store = reactive({
+  api_base: "https://api.themoviedb.org/3/search/movie",
+  api_key: 'ca6353eac5048425ca03d3a2a47014d3',
+  API_URL: "https://api.themoviedb.org/3/search/movie?api_key=ca6353eac5048425ca03d3a2a47014d3&query=",
+  movies: null,
+  searchMovies: "",
 
+  callApi(url) {
+    axios
+      .get(url)
+      .then(response => {
+        this.movies = response.data.results
+      })
+      .catch(error => {
+        console.log(error);
+        console.error(error.message);
+      })
 
-
-
-
+  },
 })
