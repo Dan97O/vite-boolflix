632ea186756558f8424193e95a2ac8429cbe8d25
Dan97O <dan97-oddo@live.it>
2023-04-20 17:42:31 +0200

    add keyup.enter input search

--- src/components/inputSearch.vue
+++ src/components/inputSearch.vue
@@ -13,10 +13,22 @@ export default {
 
 <template>
   <div>
-    <input type="text" placeholder="Ricerca Film o Serie TV" v-model="this.store.searchMovies">
+    <input type="search" placeholder="Ricerca Film o Serie TV" v-model="this.store.searchMovies"
+      @keyup.enter="$emit('moviesShow')">
     <button @click="$emit('moviesShow')"> Click </button>
   </div>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+input {
+  background-color: rgba(0, 0, 0, 0.945);
+  color: lightgrey;
+
+}
+
+button {
+  background-color: rgba(0, 0, 0, 0.945);
+  color: lightgrey;
+}
+</style>
