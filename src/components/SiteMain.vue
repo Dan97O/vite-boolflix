fe055c830f9162cbb5db445b153d6d4af8552a7c
Dan97O <dan97-oddo@live.it>
2023-04-21 16:18:53 +0200

    add message h1 main

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -19,7 +19,9 @@ export default {
       <div class="row">
         <h1 class="text-center pb-2 mt-3" v-if="store.movies.length > 0">ORIGINALI NETFLIX</h1>
         <div class="p-0" v-else>
-          <div class="view"></div>
+          <div class="view d-flex align-items-center justify-content-center">
+            <h1>Avvia la ricerca di un film o una serie Tv</h1>
+          </div>
         </div>
         <div class="col-12 d-flex flex-wrap justify-content-center">
           <MoviesList class=" overflow-scroll" />
@@ -45,6 +47,7 @@ main {
     background-color: rgba(0, 0, 0, 0.945);
     height: calc(100vh - 70px);
     overflow: hidden;
+    color: white;
   }
 }
 
